1165d5e5d07033eb1b98540c28980682b34447ef
masuda220 <masuda220@gmail.com>
2018-02-22 18:04:41 +0900

    桁数をかえた時に、longに収まるかの判定ロジックの追加

--- src/main/java/com/example/fundamentals/numeric/SmallDecimal.java
+++ src/main/java/com/example/fundamentals/numeric/SmallDecimal.java
@@ -23,14 +23,18 @@ public class SmallDecimal {
     public SmallDecimal add(SmallDecimal other) {
         long x = value;
         long y = other.value;
-
-        long sum = x + y;
-
-        if ( (((sum ^ x) & (sum ^ y)) >> 63) != 0L )
-            throw new ArithmeticException("add overflow");
+        long sum = add(x,y);
 
         return new SmallDecimal(sum, scale);
     }
+
+    private long add(long x, long y) {
+        long sum = x + y;
+        if ( (((sum ^ x) & (sum ^ y))) >= 0L) return sum; // not overflowed
+
+        throw new ArithmeticException("overflow in add");
+    }
+
 
     public SmallDecimal subtract(SmallDecimal other) {
         long x = value;
@@ -50,7 +54,7 @@ public class SmallDecimal {
 
         long product = x * y;
         if ( y != 0L && product/y != x )
-            throw new ArithmeticException("multiply overflow");
+            throw new ArithmeticException("倍数 overflow");
 
         return new SmallDecimal(product,scale);
     }
@@ -73,6 +77,32 @@ public class SmallDecimal {
         return String.format("scale:%d, value:%d", scale, value);
     }
 
+    // サポートメソッド
+    enum Shift {
+        zero(0),
+        one(1),
+        two(2),
+        three(3),
+        four(4),
+        five(5);
+
+        long 倍数 = 1; // 10 ^ 0
+        long 限界値 = Long.MAX_VALUE;
+
+        Shift(int size) {
+            if( size == 0 ) return; // 初期値
+
+            倍数 = 10 ^ size;
+            限界値 = Long.MAX_VALUE / size;
+        }
+    }
+
+    long shift(long value, Shift shift) {
+        if (Math.abs(value) <= shift.限界値) return value * shift.倍数;
+
+        throw new ArithmeticException("shift overflow");
+    }
+
     // Factory methods
     static public SmallDecimal of(int value) {
         return new SmallDecimal(value, 0);
@@ -81,4 +111,6 @@ public class SmallDecimal {
     static public SmallDecimal of(long value) {
         return new SmallDecimal(value, 0);
     }
+
+
 }
